160dbc5b4180e74e950fc41da2e461ffc6a251c1
João Luís Gonçalves Grácio <joaogojuryu@hotmail.com>
2016-08-31 12:24:04 +0100

    Que dia estranho!!! ainda nao peguei em nada... a pesquisa tambem esta esquesita como o caralho vou passar a rega, luz e crescimento para inteiros

--- ViveirosID/ViveirosID/Controllers/ArtigosController.cs
+++ ViveirosID/ViveirosID/Controllers/ArtigosController.cs
@@ -39,7 +39,11 @@ namespace ViveirosID.Controllers {
                     break;
 
                 case "nome tecnico":
-                    artigo = artigo.OrderByDescending(a => a.nometecnico);
+                    if (ordem == "ascendente") {
+                        artigo = artigo.OrderBy(a => a.nometecnico);
+                    } else if (ordem == "descendente") {
+                        artigo = artigo.OrderByDescending(a => a.nometecnico);
+                    }
                     break;
 
                 case "disponibilidade":
@@ -51,18 +55,59 @@ namespace ViveirosID.Controllers {
                     break;
 
                 case "descricao":
-                    artigo = artigo.OrderByDescending(a => a.descricao);
+                    if (ordem == "ascendente") {
+                        artigo = artigo.OrderBy(a => a.descricao.Length);
+                    } else if (ordem == "descendente") {
+                        artigo = artigo.OrderByDescending(a => a.descricao.Length);
+                    }
                     break;
 
                 case "plantacao comeca":
+                    // Listagem de mes a mes para mediante o tipo de ordenação 
+                    // concatenar de ordem ascendente ou descendente
+                    //
+                    var janeiro = artigo.Where(a => a.plantacaoComeca == "Janeiro");
+                    var fevereiro = artigo.Where(a => a.plantacaoComeca == "Fevereiro");
                     var março = artigo.Where(a => a.plantacaoComeca == "Março");
                     var abril = artigo.Where(a => a.plantacaoComeca == "Abril");
-                                                                                                    
-                    artigo = março;
+                    var maio = artigo.Where(a => a.plantacaoComeca == "Maio");
+                    var junho = artigo.Where(a => a.plantacaoComeca == "Junho");
+                    var julho = artigo.Where(a => a.plantacaoComeca == "Julho");
+                    var agosto = artigo.Where(a => a.plantacaoComeca == "Agosto");
+                    var setembro = artigo.Where(a => a.plantacaoComeca == "Setembro");
+                    var outubro = artigo.Where(a => a.plantacaoComeca == "Outubro");
+                    var novembro = artigo.Where(a => a.plantacaoComeca == "Novembro");
+                    var dezembro = artigo.Where(a => a.plantacaoComeca == "Dezembro");
+
+                    if (ordem == "ascendente") {
+                        artigo = janeiro.Concat(fevereiro).Concat(março).Concat(abril).Concat(maio).Concat(junho).Concat(julho).Concat(agosto).Concat(setembro).Concat(outubro).Concat(novembro).Concat(dezembro);
+                    } else if (ordem == "descendente") {
+                        artigo = dezembro.Concat(novembro).Concat(outubro).Concat(setembro).Concat(agosto).Concat(julho).Concat(junho).Concat(maio).Concat(abril).Concat(março).Concat(fevereiro).Concat(janeiro);
+                    }
                     break;
 
                 case "plantacao acaba":
-                    artigo = artigo.OrderByDescending(a => a.plantacaoAcaba);
+                    // Listagem de mes a mes para mediante o tipo de ordenação 
+                    // concatenar de ordem ascendente ou descendente
+                    //
+                    var janeiroA = artigo.Where(a => a.plantacaoComeca == "Janeiro");
+                    var fevereiroA = artigo.Where(a => a.plantacaoComeca == "Fevereiro");
+                    var marçoA = artigo.Where(a => a.plantacaoComeca == "Março");
+                    var abrilA = artigo.Where(a => a.plantacaoComeca == "Abril");
+                    var maioA = artigo.Where(a => a.plantacaoComeca == "Maio");
+                    var junhoA = artigo.Where(a => a.plantacaoComeca == "Junho");
+                    var julhoA = artigo.Where(a => a.plantacaoComeca == "Julho");
+                    var agostoA = artigo.Where(a => a.plantacaoComeca == "Agosto");
+                    var setembroA = artigo.Where(a => a.plantacaoComeca == "Setembro");
+                    var outubroA = artigo.Where(a => a.plantacaoComeca == "Outubro");
+                    var novembroA = artigo.Where(a => a.plantacaoComeca == "Novembro");
+                    var dezembroA = artigo.Where(a => a.plantacaoComeca == "Dezembro");
+
+                    if (ordem == "ascendente") {
+                        artigo = janeiroA.Concat(fevereiroA).Concat(marçoA).Concat(abrilA).Concat(maioA).Concat(junhoA).Concat(julhoA).Concat(agostoA).Concat(setembroA).Concat(outubroA).Concat(novembroA).Concat(dezembroA);
+                    } else if (ordem == "descendente") {
+                        artigo = dezembroA.Concat(novembroA).Concat(outubroA).Concat(setembroA).Concat(agostoA).Concat(julhoA).Concat(junhoA).Concat(maioA).Concat(abrilA).Concat(marçoA).Concat(fevereiroA).Concat(janeiroA);
+                    }
                     break;
 
                 case "peso":
@@ -74,15 +119,42 @@ namespace ViveirosID.Controllers {
                     break;
 
                 case "crescimento":
-                    artigo = artigo.OrderByDescending(a => a.crescimento);
+
+                    var itensiva = artigo.Where(a => a.crescimento == "Rápido");
+                    var moderado = artigo.Where(a => a.crescimento == "Moderado");
+                    var lento = artigo.Where(a => a.crescimento == "Lento");
+
+                    if (ordem == "ascendente") {
+                        artigo = lento.Concat(moderado).Concat(itensiva);
+                    } else if (ordem == "descendente") {
+                        artigo = itensiva.Concat(moderado).Concat(lento);
+                    }
                     break;
 
                 case "luz":
-                    artigo = artigo.OrderByDescending(a => a.Luz);
+
+                    var sol = artigo.Where(a => a.Luz == "Sol");
+                    var meiasombra = artigo.Where(a => a.Luz == "Meia-Sombra");
+                    var sombra = artigo.Where(a => a.Luz == "Sombra");
+
+                    if (ordem == "ascendente") {
+                        artigo = sombra.Concat(meiasombra).Concat(sol);
+                    } else if (ordem == "descendente") {
+                        artigo = sol.Concat(meiasombra).Concat(sombra);
+                    }
                     break;
 
                 case "rega":
-                    artigo = artigo.OrderByDescending(a => a.Rega);
+
+                    var itensivaA = artigo.Where(a => a.Rega == "Intensiva");
+                    var regular = artigo.Where(a => a.Rega == "Regular");
+                    var reduzida = artigo.Where(a => a.Rega == "Reduzida");
+
+                    if (ordem == "ascendente") {
+                        artigo = reduzida.Concat(regular).Concat(itensivaA);
+                    } else if (ordem == "descendente") {
+                        artigo = itensivaA.Concat(regular).Concat(reduzida);
+                    }
                     break;
 
                 case "preço":
@@ -118,7 +190,274 @@ namespace ViveirosID.Controllers {
         
         }
 
-        
+        public ActionResult Pesquisa() {
+
+            // Prepara a lista standart de artigos para enviar para a View
+            //
+            var artigo = (from umArtigo in db.Artigo
+                          select umArtigo).Include(a => a.Categoria);
+
+            // Recolhe o valor mais alto de preço e o mais baixo
+            //
+            ViewBag.primeiro_preço = artigo.OrderBy(a => a.preço).FirstOrDefault().preço;
+
+            ViewBag.ultimo_preço = artigo.OrderByDescending(a => a.preço).FirstOrDefault().preço;
+
+            // Recolhe o valor mais alto de peso e o mais baixo
+            //
+            ViewBag.primeiro_peso = artigo.OrderBy(a => a.peso).FirstOrDefault().peso;
+
+            ViewBag.ultimo_peso = artigo.OrderByDescending(a => a.peso).FirstOrDefault().peso;
+
+            // Recolhe as três palavras de escolha de Crescimento
+            // Rápido, Moderado, Lento
+            //
+            ViewBag.crescimento = new string[3] { "Lento", "Moderado", "Rapido" };
+
+            // Recolhe as três palavras relacionadas com a Luz 
+            // Sol, Meia-Sombra, Sombra
+            //
+            ViewBag.luz = new string[3] { "Sombra", "Meia-Sombra", "Sol" };
+
+            // Recolhe as três palavras relacionadas com a Rega
+            // Intensiva, Regula, Reduzida
+            //
+            ViewBag.rega = new string[3] { "Reduzida", "Regular", "Intensiva" };
+
+            return View(artigo.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Pesquisa(string nome, string peso, string preço, string crescimento, string luz, string rega, string ordem) {
+            
+            // Faz uma listagem "usual" de todos os artigos 
+            // listagem esta que vai depois ser filtrada e apresentada
+            //
+            /*var artigo = (from umArtigo in db.Artigo
+                          select umArtigo).Include(a => a.Categoria);*/
+
+            // Separa a string peso => "de 2 gramas até 8 gramas" pelo caracter ' '
+            //
+            var separa_por_espaço_arr = peso.Split(' ');
+
+            // Regista dois inteiros relativos ao limite inferior e superior do preço em gramas
+            //
+            var primeiro_peso = Int32.Parse(separa_por_espaço_arr[1]);
+
+            var segundo_peso = Int32.Parse(separa_por_espaço_arr[4]);
+
+
+            // Separa a string preço => "de €2 até €8" pelo caracter €
+            // de forma a recolher os numeros referentes aos preços
+            // o resultados deste split é o seguinte: ["de "]["2 até "]["8"]
+            //
+            var separa_por_euro_arr = preço.Split('€');
+
+            // Separa a string ["2 até "] pelo caracter ' ' de forma a isolar o numero
+            //
+            var primeiro_valor_arr = separa_por_euro_arr[1].Split(' ');
+
+            // Converte a string que contem o primeiro preço para int
+            //
+            var primeiro_preço = float.Parse(primeiro_valor_arr[0]);
+
+            // Convert a string que contem o segundo preço para int
+            //
+            var segundo_preço = float.Parse(separa_por_euro_arr[2]);
+
+            // Separa a string relativa ao crescimento "de Lento até Rapido"
+            // Recolhe o primeiro e ultimo valor para crescimento
+            //
+            var separa_crescimento = crescimento.Split(' ');
+
+            var primeiro_crescimento = separa_crescimento[1];
+
+            var ultimo_crescimento = separa_crescimento[3];
+
+            // Separa a string relativa ao luz "de Sombra até Sol"
+            // Recolhe o primeiro e ultimo valor para crescimento
+            //
+            var separa_luz = luz.Split(' ');
+
+            var primeira_luz = separa_luz[1];
+
+            var ultima_luz = separa_luz[3];
+
+            // Separa a string relativa ao rega "de Reduzida até Intensiva"
+            // Recolhe o primeiro e ultimo valor para rega
+            //
+            var separa_rega = rega.Split(' ');
+
+            var primeira_rega = separa_rega[1];
+
+            var ultima_rega = separa_rega[3];
+
+
+            // SIMULAÇAO DA AQUISIÇÃO DE DADOS DE TODOS OS TIPOS DE crescimento, rega, luz
+            var str_crescimento = new string[3]{ "Lento", "Moderado", "Rapido" };
+            var str_luz = new string[3] { "Sombra", "Meia-Sombra", "Sol" };
+            var str_rega = new string[3] { "Reduzida", "Regular", "Intensiva" };
+            // SIMULAÇAO DA AQUISIÇÃO DE DADOS DE TODOS OS TIPOS DE crescimento, rega, luz
+
+            var primeiro_crescimento_pos = 0;
+
+            var ultimo_crescimento_pos = 0;
+
+            var primeira_luz_pos = 0;
+
+            var ultima_luz_pos = 0;
+
+            var primeira_rega_pos = 0;
+
+            var ultima_rega_pos = 0;
+
+            // Encontra a posição da primeiro_crescimento e ultimo_crescimento
+            // para atribuir o valor da posição correpondente do array str_crescimento
+            // em primeiro_crescimento_pos e ultimo_crescimento_pos
+            //
+            for (var i = 0; i <= str_crescimento.Length - 1; i++) {
+                if (str_crescimento[i].Equals(primeiro_crescimento)) {
+                    primeiro_crescimento_pos = i;
+                }
+                if (str_crescimento[i].Equals(ultimo_crescimento)) {
+                    ultimo_crescimento_pos = i;
+                }
+            }
+
+            // Encontra a posição da primeira_rega e ultima_rega
+            // para atribuir o valor dessas posições correspondentes ao str_rega
+            // em primeira_rega_pos e ultima_rega_pos
+            //
+            for (var i = 0; i <= str_rega.Length - 1; i++) {
+                if (str_rega[i].Equals(primeira_rega)) {
+                    primeira_rega_pos = i;
+                }
+                if (str_rega[i].Equals(ultima_rega)) {
+                    ultima_rega_pos = i;
+                }
+            }
+
+            // Encontra a posição da primeira_luz e ultima_luz
+            // para atribuir o valor dessas posições correspondentes ao str_luz
+            // em primeira_luz_pos e ultima_luz_pos
+            //
+            for (var i = 0; i <= str_luz.Length - 1; i++) {
+                if (str_luz[i].Equals(primeira_luz)) {
+                    primeira_luz_pos = i;
+                }
+                if (str_luz[i].Equals(ultima_luz)) {
+                    ultima_luz_pos = i;
+                }
+            }
+
+            // IMPLEMENTAÇÃO DE TODOS OS FILTROS E CRIAÇÃO DE VIEWBAGS PARA ENVIAR COM OS LIMITOS DOS FILTROS PARA A VIEW
+
+            // Faz uma selecao de todos os artigos
+            // para usar de forma auxilar
+            //
+            var todo_artigo = (from umArtigo in db.Artigo
+                               select umArtigo);
+
+            var artigo = (from umArtigo in db.Artigo
+                          where umArtigo.crescimento == "Maluco"
+                          select umArtigo);
+
+            // ISTO PODIA SER FEITO COM RECURSO A NUMEROS PARA LUZ, REGA, CRESCIMENTO MAS EU FUI TEIMOSO 
+            // E CONTINUEI O PROCESSO ASSIM
+
+            // Faz a filtragem por luz
+            //
+            for (var i = primeira_luz_pos; i <= ultima_luz_pos; i++) {
+                var str_pos = str_luz[i];
+                artigo = artigo.Concat(todo_artigo.Where(a => a.Luz.Equals(str_pos)));
+                /*try {
+                    var nome_str = artigo_not_null.FirstOrDefault().nome;
+                    artigo = artigo_not_null;
+                } catch (System.NullReferenceException ex) {
+                    
+                }*/
+            }
+
+            // Faz a filtragem por rega
+            //
+            for (var i = primeira_rega_pos; i <= ultima_rega_pos; i++) {
+                var str_pos = str_rega[i];
+                artigo = artigo.Concat(todo_artigo.Where(a => a.Rega.Equals(str_pos)));
+                /*try {
+                    var nome_str = artigo_not_null.FirstOrDefault().nome;
+                    artigo = artigo_not_null;
+                } catch (System.NullReferenceException ex) {
+
+                }*/
+            }
+
+            // Faz a filtragem por crescimento
+            //
+            for (var i = primeiro_crescimento_pos; i <= ultimo_crescimento_pos; i++) {
+                var str_pos = str_luz[i];
+                artigo = artigo.Concat(todo_artigo.Where(a => a.crescimento.Equals(str_pos)));
+                /*try {
+                    var nome_str = artigo_not_null.FirstOrDefault().nome;
+                    artigo = artigo_not_null;
+                } catch (System.NullReferenceException ex) {
+
+                }*/
+            }
+
+            // Faz uma pesquisa da palavra a encontrar pesquisa em cada um dos artigos se existe uma correspondencia para nome
+            //
+            if (nome != null) {
+                artigo = artigo.Where(a => a.nome.Contains(nome) || a.nometecnico.Contains(nome) || a.descricao.Contains(nome));
+                /*artigo = artigo.Concat(artigo.Where(a => a.nometecnico.Contains(nome)));
+                artigo = artigo.Concat(artigo.Where(a => a.descricao.Contains(nome)));*/
+                
+            }
+
+            // Implementa dois filtros para peso
+            //
+            artigo = artigo.Where(a => a.peso >= primeiro_peso);
+            artigo = artigo.Where(a => a.peso <= segundo_peso);
+
+            // Implementa dois filtros para preço
+            artigo = artigo.Where(a => a.preço >= primeiro_preço);
+            artigo = artigo.Where(a => a.preço <= segundo_preço);
+
+            /*if (ordem == "descendente") {
+                artigo = artigo.OrderByDescending(a => a.nome);
+            } else if (ordem == "ascendente") {
+                artigo = artigo.OrderBy(a => a.nome);
+            }*/
+
+            // Recolhe o valor mais alto de preço e o mais baixo
+            //
+            ViewBag.primeiro_preço = todo_artigo.OrderBy(a => a.preço).FirstOrDefault().preço;
+
+            ViewBag.ultimo_preço = todo_artigo.OrderByDescending(a => a.preço).FirstOrDefault().preço;
+
+            // Recolhe o valor mais alto de peso e o mais baixo
+            //
+            ViewBag.primeiro_peso = todo_artigo.OrderBy(a => a.peso).FirstOrDefault().peso;
+
+            ViewBag.ultimo_peso = todo_artigo.OrderByDescending(a => a.peso).FirstOrDefault().peso;
+
+            // Recolhe as três palavras de escolha de Crescimento
+            // Rápido, Moderado, Lento
+            //
+            ViewBag.crescimento = new string[3] { "Lento", "Moderado", "Rapido" };
+
+            // Recolhe as três palavras relacionadas com a Luz 
+            // Sol, Meia-Sombra, Sombra
+            //
+            ViewBag.luz = new string[3] { "Sombra", "Meia-Sombra", "Sol"};
+
+            // Recolhe as três palavras relacionadas com a Rega
+            // Intensiva, Regula, Reduzida
+            //
+            ViewBag.rega = new string[3] { "Reduzida", "Regular", "Intensiva" };
+
+            return View(artigo.ToList());
+        }
+
         // GET: Artigoes/Details/5
         [Authorize]
         public ActionResult Details(int? id) {

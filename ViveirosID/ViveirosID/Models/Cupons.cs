506385b07bf598592bbd2efed471771b0ee92ef5
JoaoGracio <joaogojuryu@hotmail.com>
2018-04-08 09:18:17 +0100

    Esquema de base de dados actualizado e powerpoint

--- ViveirosID/ViveirosID/Models/Cupons.cs
+++ ViveirosID/ViveirosID/Models/Cupons.cs
@@ -1,3 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ViveirosID.Models
+{
+    public class Cupons
+    {
+        // Inicializar as variaveis ListaDeCompras, ListaDeArtigos, ListaDeImagens
+        public Cupons()
+        {
+            ListaDeCuponsArtigos = new HashSet<CuponsArtigos>();
+        }
+
         [Key]
         public int CuponsID { get; set; }
         public string codigo { get; set; }
